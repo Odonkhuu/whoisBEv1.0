0d773d790483e10658be20af1aede4f827cd4250
Odonkhuu <135576836+Odonkhuu@users.noreply.github.com>
2023-06-21 17:36:59 +0800

    nuuts vg sergeekh
    n

--- whoisBEv10/app1/user.py
+++ whoisBEv10/app1/user.py
@@ -78,7 +78,18 @@ def userLoginView(request):
     return HttpResponse(json.dumps(resp), content_type="application/json")
 #   userLoginView end
 
-
+# nuuts vg sergeeh hesgiin
+def forgetView(request):
+    jsons = json.loads(request.body)
+    email = jsons['email']
+    newPass1 = jsons['newPass1']
+    newPass2 = jsons['newPass2']
+    resp = {}
+    resp["email"] = email
+    resp["newPass1"] = newPass1        
+    resp["newPass2"] = newPass2        
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+####################################
 
 def userRegisterView(request):
     jsons = json.loads(request.body)
